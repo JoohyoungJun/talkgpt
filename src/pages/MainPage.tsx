c5bbc504d0de6d47d48bf0e261d588ef8fd19f5a
JoohyoungJun <joohyoung.jun@stonybrook.edu>
2025-07-15 22:37:32 +0900

    mainpage.tsx api key debugging log eliminated

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -11,7 +11,7 @@ interface Message {
 const fetchGeminiReply = async (userInput: string): Promise<string> => {
     const apikey = import.meta.env.VITE_GEMINI_API_KEY;
 
-    console.log("API_Key: ", import.meta.env.VITE_GEMINI_API_KEY);
+    //console.log("API_Key: ", import.meta.env.VITE_GEMINI_API_KEY);
 
     const response = await fetch (
         `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-002:generateContent?key=${apikey}`,
